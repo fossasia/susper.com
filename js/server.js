89854188051e276e5f93fb993537a7ea5447eb7d
Michael Peter Christen <mc@yacy.net>
2013-03-19 18:19:07 +0100

    full redesign - using now only models and collections from backbone.js

--- js/server.js
+++ js/server.js
@@ -9,14 +9,203 @@
 
 var server="localhost:8090";
 
+
 var SearchModel = Backbone.Model.extend({
     urlRoot:'http://' + server + '/solr/select?hl=false&wt=yjson&facet=true&facet.mincount=1&facet.field=url_file_ext_s&callback=?',
     defaults:{query:'',start:'0',rows:'100'},
+
     url:function(){
         return this.urlRoot + '&query=' + this.attributes.query + '&start=' + this.attributes.start + '&rows=' + this.attributes.rows;
     },
+
     parse:function(resp){
         return resp[0].channels[0];
+    },
+
+    rowCollection:function(){
+        var rc = new RowCollection();
+        rc.add(this.attributes.items);
+        return rc;
+    },
+
+    totalResults:function(){
+        var tr = this.attributes.totalResults.replace(/[,.]/,"");
+        if (this.rowCollection().length > tr) tr = this.rowCollection().length;
+        return tr;
+    },
+
+    navigationCollection:function(){
+        var navigationCollection = new NavigationCollection();
+        navigationCollection.add(this.attributes.navigation);
+        return navigationCollection;
+    },
+
+    fullPageNavigation:function(maximumRecords) {
+        var html = "";
+        var tr = this.totalResults();
+        if (tr > 0) {
+          html += "<p>" + this.rowCollection().length + " results from a total of " + tr + " docs in index; search time: " + ((new Date()).getTime() - start.getTime()) + " milliseconds.</p>";
+          html += "<div class=\"nav-wrapper\"><nav class=\"ym-vlist\"><h6 class=\"ym-vtitle\">Result Size</h6><dl>";
+          if (maximumRecords == 10) html += "<dt><input type=\"checkbox\" checked=\"checked\"></dt><dd>10 results</dd>";
+          if (maximumRecords != 10 && tr >= 10) html += "<dt><input type=\"checkbox\" onClick=\"window.location.href='yacytable.html?query=" + this.attributes.query + "&startRecord=" + this.attributes.start + "&maximumRecords=" + 10 + "'\"></dt><dd>10 results</dd>";
+          if (maximumRecords == 100) html += "<dt><input type=\"checkbox\" checked=\"checked\"></dt><dd>100 results</dd>";
+          if (maximumRecords != 100 && tr >= 100) html += "<dt><input type=\"checkbox\" onClick=\"window.location.href='yacytable.html?query=" + this.attributes.query + "&startRecord=" + this.attributes.start + "&maximumRecords=" + 100 + "'\"></dt><dd>100 results</dd>";
+          if (maximumRecords == 1000) html += "<dt><input type=\"checkbox\" checked=\"checked\"></dt><dd>1000 results</dd>";
+          if (maximumRecords != 1000 && tr >= 1000) html += "<dt><input type=\"checkbox\" onClick=\"window.location.href='yacytable.html?query=" + this.attributes.query + "&startRecord=" + this.attributes.start + "&maximumRecords=" + 1000 + "'\"></dt><dd>1000 results</dd>";
+          if (tr <= 10000 && maximumRecords < tr && tr > 100) html += "<dt><input type=\"checkbox\" onClick=\"window.location.href='yacytable.html?query=" + this.attributes.query + "&startRecord=" + this.attributes.start + "&maximumRecords=" + tr + "'\"></dt><dd>all " + tr + " results</dd>";
+          html += "</dl></nav></div>";
+          return html;
+        }
+    }
+});
+
+var script = "";
+var RowModel = Backbone.Model.extend({
+
+  tablerow:function(type) {
+    if (this.attributes.link == null) return "";
+    var link = this.attributes.link;
+    var protocol = "";
+    var host = "";
+    // extract the path
+    var path = link;
+    var file = "";
+    var p = link.indexOf("//");
+    if (p > 0) {
+      protocol = link.substring(0, p - 1);
+      q = link.indexOf("/", p + 2);
+      if (q > 0) {
+        host = link.substring(p + 2, q);
+        path = link.substring(q);
+      } else {
+        host = link.substring(p + 2);
+        path = "/";
+      }
+    }
+    var q = path.lastIndexOf("/");
+    if (q > 0) {
+      file = path.substring(q + 1);
+      path = path.substring(0, q + 1);
+    } else {
+      file = path;
+      path = "/";
+    }
+    var title = this.get("title");
+    path = unescape(path);
+    var origpath = path; // save this for later in the link, this may be shortened now
+    if (path.length >= 40) path = path.substring(0, 18) + "..." + path.substring(path.length - 19);
+    if (title == "") title = path;
+    if (title.length >= 60) title = title.substring(0, 28) + "..." + title.substring(title.length - 29);
+    var pd = this.get("pubDate");
+    if (pd == undefined) pd = "";
+    var comma = pd.indexOf(",");
+    if (comma > 0) pd = pd.substring(comma + 2);
+    if (pd.substring(pd.length - 6) == " +0000") pd = pd.substring(0, pd.length - 6);
+    pd = pd.replace(" ","&nbsp;").replace(" ","&nbsp;").replace(" ","&nbsp;");
+    
+    // update download script
+    if (link.indexOf("smb://") >= 0) script += "smbget -n -a -r \"" + link + "\"\n"; else script += "curl -OL \"" + link + "\"\n";
+    
+    // make table row
+    var html = "";
+    if (type == "row") {
+      html += "<tr>";
+      html += "<td><a class=\"searchresults\" href=\"" + protocol + "://" + host + "/" + "\">" + protocol + "://" + host + "</a></td>"; // Host
+      html += "<td><a class=\"searchresults\" href=\"" + protocol + "://" + host + origpath + "\">" + path + "</a></td>"; // Path
+      if (file.length == 0 || file == "/") file = "[index-file]";
+      html += "<td><a class=\"searchresults\" href=\"" + link + "\">" + file + "</a></td>"; // URL
+      if (this.get("sizename") == "-1 byte") html += "<td></td>"; else html += "<td align=\"right\">" + this.get("sizename") + "</td>"; // Size
+      html += "<td align=\"right\">" + pd + "</td>"; // Date
+      html += "</tr>";
+    }
+    if (type == "image") {
+      var name = title;
+      while ((p = name.indexOf("/")) >= 0) { name = name.substring(p + 1); }
+      html += "<a href=\"" + link + "\" name=\"" + name + "\">";
+      html += "<img src=\"" + link + "\" width=\"96\" height=\"96\" />";
+      html += "</a>";
+    }
+      
+    // return entry
+    return html;
+}
+});
+
+var RowCollection = Backbone.Collection.extend({
+  model: RowModel,
+
+  resultTable:function() {
+    var html = "";
+    if (this.length > 0) {
+      document.getElementById("searchnavigation").innerHTML = "<p>found " + this.length + " documents, preparing table...</p>";
+      html += "<table class=\"narrow\">";
+      html += "<thead><tr><th>Host</td><th>Path</td><th>File</td><th>Size</td><th>Date</td></tr></thead><tbody>";
+      for (var i = 0; i < this.length; i++) { html += this.at(i).tablerow("row"); }
+      html += "</tbody></table>";
+    }
+    return html;
+  },
+
+  resultImages:function() {
+    var html = "";
+    document.getElementById("searchnavigation").innerHTML = "<p>found " + this.length + " images, preparing...</p>";
+    for (var i = 0; i < this.length; i++) { html += this.at(i).tablerow("image"); }
+    return html;
+  }
+});
+
+var FacetModel = Backbone.Model.extend({
+
+    facetElements:function() {
+        var elts = {};
+        for (var fc = 0; fc < this.attributes.elements.length; fc++) {
+          elts[this.attributes.elements[fc].name] = this.attributes.elements[fc].count;
+        }
+        return elts;
+    },
+
+    facetBox:function(facetName, modifier, query, startRecord, maximumRecords) {
+      var html = "";
+
+      var extnav = "<div class=\"nav-wrapper\"><nav class=\"ym-vlist\"><h6 class=\"ym-vtitle\">" + facetName + "</h6><dl>";
+      var ftc = 0;
+      var filetypes = this.facetElements();
+      for (var key in filetypes) {
+          if (filetypes[key] > 0)  {
+            extnav += "<dt><input type=\"radio\" name=\"filetypes\" value=\"" + key + "\" onClick=\"window.location.href='yacytable.html?query=" + query;
+            extnav += " filetype:" + key + "&startRecord=" + startRecord + "&maximumRecords=" + maximumRecords + "'\"></dt><dd>" + key + " (" + filetypes[key] + ")</dd>";
+            ftc++;
+          }
+      }
+      extnav += "</dl></nav></div>";
+
+      if (ftc > 1) {
+          html += extnav;
+      } else {
+          // check if there is a filetype constraint and offer a removal
+          if (modifier != "") {
+            html += "<div class=\"nav-wrapper\"><nav class=\"ym-vlist\"><h6 class=\"ym-vtitle\">Filetype</h6><dl>";
+            html += "<dt><input type=\"checkbox\" checked=\"checked\" name=\"filetypes\" value=\"" + key;
+            html += "\" onClick=\"window.location.href='yacytable.html?query=" + query.substring(0, query.length - modifier.length - 1);
+            html += "&startRecord=" + startRecord + "&maximumRecords=" + maximumRecords + "'\"></dt><dd>" + modifier + " (remove)</dd>";
+            html += "</dl></nav></div>";
+          }
+      }
+
+      return html;
+    }
+});
+
+var NavigationCollection = Backbone.Collection.extend({
+    model:FacetModel,
+
+    facet:function(name){
+        for (i = 0; i < this.length; i++) {
+            var facet = this.at(i);
+            var facetname = facet.attributes.facetname;
+            if (facetname == name) return facet;
+        }
+        return null;
     }
 });
 
@@ -27,7 +216,7 @@ function execGet() {
     if (startRecord == null) startRecord = 0; else startRecord = startRecord[1];
     var maximumRecords = new RegExp("[\\?&]maximumRecords=([^&#]*)").exec(window.location.href);
     if (maximumRecords == null) maximumRecords = 10; else maximumRecords = maximumRecords[1];
-    document.getElementById("query").value=query[1].replace(/%20/g," ");
+    document.getElementById("query").value=query[1].replace(/%20/g," ").replace(/\+/g," ");
     document.getElementById("maximumRecords").value=maximumRecords;
     document.getElementById("startRecord").value=startRecord;
     search(document.getElementById("query").value, maximumRecords, startRecord);
--- js/yacytable.js
+++ js/yacytable.js
@@ -8,19 +8,6 @@
  */
 
 
-// static variables
-var start = new Date();
-var query = "";
-var maximumRecords = "1000";
-var startRecord = "0";
-var searchresult;
-var totalResults = 0;
-var filetypes;
-var topics;
-var script = "";
-var modifier = "";
-var modifiertype = "";
-
 function search(search, count, offset) {
   var navhtml = document.getElementById("searchnavigation");
   if (navhtml != null) navhtml.innerHTML = "<p>loading...</p>";
@@ -38,52 +25,43 @@ function search(search, count, offset) {
   var search = new SearchModel({query:search,start:offset,rows:count}); 
   search.fetch({
     success:function(search) {
-        preparepage(search.attributes);
+      document.getElementById("searchnavigation").innerHTML = "<p>parsing result...</p>";
+      var rowCollection = new RowCollection();
+      rowCollection.add(search.attributes.items);
+      var totalResults = search.attributes.totalResults.replace(/[,.]/,"");
+      var navigation = search.navigationCollection();
+  
+      // update navigation
+      //var topics = navigation.facet("topics");
+      var filetypefacet = navigation.facet("filetypes");
+      var filetypes = {};
+      if (filetypefacet) {
+        filetypes = filetypefacet.facetElements();
+      }
+      for (var key in filetypes) {
+        if (query.indexOf("filetype:" + key) >= 0) delete filetypes[key];
+      }
+
+      script = ""; // this is a static global
+      var modifier = "";
+      var modifiertype = "";
+      for (var extl = 2; extl < 6; extl++) {
+        if (query.length >= 13 && query.substring(query.length - 10 - extl, query.length - extl) == " filetype:") {
+          modifier = query.substring(query.length - 9 - extl);
+          modifiertype = modifier.substring(modifier.length - extl);
+          break;
+        }
+      }
+
+      if (modifiertype == "png" || modifiertype == "gif" || modifiertype == "jpg" || modifiertype == "PNG" || modifiertype == "GIF" || modifiertype == "JPG") {
+        document.getElementById("searchresults").innerHTML = rowCollection.resultImages();
+      } else {
+        document.getElementById("searchresults").innerHTML = rowCollection.resultTable();
+      }
+      document.getElementById("searchnavigation").innerHTML = search.fullPageNavigation(maximumRecords) + filetypefacet.facetBox("Filetype", modifier, query, startRecord, maximumRecords)
+      hideDownloadScript();
     }
   });
-}
-
-function navget(list, name) {
-  for (var i = 0; i < list.length; i++) {
-    if (list[i].facetname == name) return list[i];
-  }
-}
-
-function preparepage(firstChannel) {
-  document.getElementById("searchnavigation").innerHTML = "<p>parsing result...</p>";
-  searchresult = firstChannel.items;
-  totalResults = firstChannel.totalResults.replace(/[,.]/,"");
-  topics = navget(firstChannel.navigation, "topics");
-  filetypefacet = navget(firstChannel.navigation, "filetypes");
-  
-  filetypes = {};
-  if (filetypefacet) {
-    var elements = filetypefacet.elements;
-    for (var fc = 0; fc < elements.length; fc++) {
-        filetypes[elements[fc].name] = elements[fc].count;
-    }
-  }
-
-  script = "";
-  for (var extl = 2; extl < 6; extl++) {
-    if (query.length >= 13 && query.substring(query.length - 10 - extl, query.length - extl) == " filetype:") {
-      modifier = query.substring(query.length - 9 - extl);
-      modifiertype = modifier.substring(modifier.length - extl);
-      break;
-    }
-  }
-
-  if (modifiertype == "png" || modifiertype == "gif" || modifiertype == "jpg" || modifiertype == "PNG" || modifiertype == "GIF" || modifiertype == "JPG") {
-    var tt = resultImages();
-    document.getElementById("searchresults").innerHTML = tt;
-  } else {
-    var tt = resultList();
-    document.getElementById("searchresults").innerHTML = tt;
-  }
-  var tt = resultNavigation();
-  document.getElementById("searchnavigation").innerHTML = tt;
-  //document.getElementById("serverlist").innerHTML = "";
-  hideDownloadScript();
 }
 
 function makeDownloadScript() {
@@ -95,154 +73,4 @@ function hideDownloadScript() {
   document.getElementById("downloadscript").innerHTML = "";
   var dlb = document.getElementById("downloadbutton");
   if (dlb) dlb.innerHTML = "<input type=\"button\" value=\"create a download script\" onClick=\"makeDownloadScript();\"/>";
-}
-
-function resultNavigation() {
-  var html = "";
-  if (searchresult.length > totalResults) totalResults = searchresult.length;
-  if (totalResults > 0) {
-      html += "<p>" + searchresult.length + " results from a total of " + totalResults + " docs in index; search time: " + ((new Date()).getTime() - start.getTime()) + " milliseconds.</p>";
-      html += "<div class=\"nav-wrapper\"><nav class=\"ym-vlist\"><h6 class=\"ym-vtitle\">Result Size</h6><dl>";
-      if (maximumRecords == 10) html += "<dt><input type=\"checkbox\" checked=\"checked\"></dt><dd>10 results</dd>";
-      if (maximumRecords != 10 && totalResults >= 10) html += "<dt><input type=\"checkbox\" onClick=\"window.location.href='yacytable.html?query=" + query + "&startRecord=" + startRecord + "&maximumRecords=" + 10 + "'\"></dt><dd>10 results</dd>";
-      if (maximumRecords == 100) html += "<dt><input type=\"checkbox\" checked=\"checked\"></dt><dd>100 results</dd>";
-      if (maximumRecords != 100 && totalResults >= 100) html += "<dt><input type=\"checkbox\" onClick=\"window.location.href='yacytable.html?query=" + query + "&startRecord=" + startRecord + "&maximumRecords=" + 100 + "'\"></dt><dd>100 results</dd>";
-      if (maximumRecords == 1000) html += "<dt><input type=\"checkbox\" checked=\"checked\"></dt><dd>1000 results</dd>";
-      if (maximumRecords != 1000 && totalResults >= 1000) html += "<dt><input type=\"checkbox\" onClick=\"window.location.href='yacytable.html?query=" + query + "&startRecord=" + startRecord + "&maximumRecords=" + 1000 + "'\"></dt><dd>1000 results</dd>";
-      if (totalResults <= 10000 && maximumRecords < totalResults && totalResults > 100) html += "<dt><input type=\"checkbox\" onClick=\"window.location.href='yacytable.html?query=" + query + "&startRecord=" + startRecord + "&maximumRecords=" + totalResults + "'\"></dt><dd>all " + totalResults + " results</dd>";
-      html += "</dl></nav></div>";
-  } else {
-      if (query == "") {
-        html += "<p>please enter some search words or use the following predefined search queries:</p>";
-        html += "<div class=\"nav-wrapper\"><nav class=\"ym-vlist\"><h6 class=\"ym-vtitle\">Find Images</h6><dl>";
-        html += "<dt>&nbsp;</dt><dd><input type=\"button\" value=\"png\" onClick=\"window.location.href='yacytable.html?query=png+filetype:png&startRecord=" + startRecord + "&maximumRecords=" + 100 + "'\"/></dd>";
-        html += "<dt>&nbsp;</dt><dd><input type=\"button\" value=\"gif\" onClick=\"window.location.href='yacytable.html?query=gif+filetype:gif&startRecord=" + startRecord + "&maximumRecords=" + 100 + "'\"/></dd>";
-        html += "<dt>&nbsp;</dt><dd><input type=\"button\" value=\"jpg\" onClick=\"window.location.href='yacytable.html?query=jpg+filetype:jpg&startRecord=" + startRecord + "&maximumRecords=" + 100 + "'\"/></dd>";
-        html += "</dl></nav></div>";
-        html += "<div class=\"nav-wrapper\"><nav class=\"ym-vlist\"><h6 class=\"ym-vtitle\">Find Document</h6><dl>";
-        html += "<dt>&nbsp;</dt><dd><input type=\"button\" value=\"pdf\" onClick=\"window.location.href='yacytable.html?query=pdf+filetype:pdf&startRecord=" + startRecord + "&maximumRecords=" + 100 + "'\"/></dd>";
-        html += "<dt>&nbsp;</dt><dd><input type=\"button\" value=\"xls\" onClick=\"window.location.href='yacytable.html?query=xls+filetype:xls&startRecord=" + startRecord + "&maximumRecords=" + 100 + "'\"/></dd>";
-        html += "<dt>&nbsp;</dt><dd><input type=\"button\" value=\"doc\" onClick=\"window.location.href='yacytable.html?query=doc+filetype:doc&startRecord=" + startRecord + "&maximumRecords=" + 100 + "'\"/></dd>";
-        html += "</dl></nav></div>";
-      } else {
-         html += "no results";
-      }
-  }
-
-  // add extension navigation
-  var extnav = "";
-  extnav += "<div class=\"nav-wrapper\"><nav class=\"ym-vlist\"><h6 class=\"ym-vtitle\">Filetype</h6><dl>";
-  var ftc = 0;
-  for (var key in filetypes) {
-      if (filetypes[key] > 0)  {
-        extnav += "<dt><input type=\"radio\" name=\"filetypes\" value=\"" + key + "\" onClick=\"window.location.href='yacytable.html?query=" + query + " filetype:" + key + "&startRecord=" + startRecord + "&maximumRecords=" + maximumRecords + "'\"></dt><dd>" + key + " (" + filetypes[key] + ")</dd>";
-        ftc++;
-      }
-  }
-  extnav += "</dl></nav></div>";
-  if (ftc > 0) {
-      html += extnav;
-  } else {
-      // check if there is a filetype constraint and offer a removal
-      if (modifier != "") {
-        html += "<div class=\"nav-wrapper\"><nav class=\"ym-vlist\"><h6 class=\"ym-vtitle\">Filetype</h6><dl>";
-        html += "<dt><input type=\"checkbox\" checked=\"checked\" name=\"filetypes\" value=\"" + key + "\" onClick=\"window.location.href='yacytable.html?query=" + query.substring(0, query.length - 13) + "&startRecord=" + startRecord + "&maximumRecords=" + maximumRecords + "'\"></dt><dd>" + modifier + " (remove)</dd>";
-        html += "</dl></nav></div>";
-      }
-  }
-
-  return html;
-}
-
-function resultList() {
-  var html = "";
-  if (searchresult.length > 0) {
-    document.getElementById("searchnavigation").innerHTML = "<p>found " + searchresult.length + " documents, preparing table...</p>";
-    html += "<table class=\"narrow\">";
-    html += "<thead><tr><th>Host</td><th>Path</td><th>File</td><th>Size</td><th>Date</td></tr></thead><tbody>";
-    for (var i = 0; i < searchresult.length; i++) { html += resultLine("row", searchresult[i], i + 1); }
-    html += "</tbody></table>";
-  }
-  return html;
-}
-
-function resultImages() {
-  var html = "";
-  document.getElementById("searchnavigation").innerHTML = "<p>found " + searchresult.length + " images, preparing...</p>";
-  for (var i = 0; i < searchresult.length; i++) { html += resultLine("image", searchresult[i]); }
-  return html;
-}
-
-function resultLine(type, item, linenumber) {
-  // evaluate item
-  if (item == null) return "";
-  if (item.link == null) return "";
-  protocol = "";
-  host = "";
-  // extract the path
-  path = item.link;
-  file = "";
-  p = item.link.indexOf("//");
-  if (p > 0) {
-    protocol = item.link.substring(0, p - 1);
-    q = item.link.indexOf("/", p + 2);
-    if (q > 0) {
-      host = item.link.substring(p + 2, q);
-      path = item.link.substring(q);
-    } else {
-      host = item.link.substring(p + 2);
-      path = "/";
-    }
-  }
-  q = path.lastIndexOf("/");
-  if (q > 0) {
-    file = path.substring(q + 1);
-    path = path.substring(0, q + 1);
-  } else {
-    file = path;
-    path = "/";
-  }
-  title = item.title;
-  path = unescape(path);
-  var origpath = path; // save this for later in the link, this may be shortened now
-  if (path.length >= 40) path = path.substring(0, 18) + "..." + path.substring(path.length - 19);
-  if (title == "") title = path;
-  if (title.length >= 60) title = title.substring(0, 28) + "..." + title.substring(title.length - 29);
-  pd = item.pubDate;
-  if (pd == undefined) pd = "";
-  var comma = pd.indexOf(",");
-  if (comma > 0) pd = pd.substring(comma + 2);
-  if (pd.substring(pd.length - 6) == " +0000") pd = pd.substring(0, pd.length - 6);
-  pd = pd.replace(" ","&nbsp;").replace(" ","&nbsp;").replace(" ","&nbsp;");
-
-  // update navigation
-  for (var key in filetypes) {
-    if (query.indexOf("filetype:" + key) >= 0) delete filetypes[key];
-  }
-  
-  // update download script
-  if (item.link.indexOf("smb://") >= 0) script += "smbget -n -a -r \"" + item.link + "\"\n"; else script += "curl -OL \"" + item.link + "\"\n";
-  
-  // make table row
-  var html = "";
-  if (type == "row") {
-    html += "<tr>";
-    html += "<td><a class=\"searchresults\" href=\"" + protocol + "://" + host + "/" + "\">" + protocol + "://" + host + "</a></td>"; // Host
-    html += "<td><a class=\"searchresults\" href=\"" + protocol + "://" + host + origpath + "\">" + path + "</a></td>"; // Path
-    if (file.length == 0 || file == "/") file = "[index-file]";
-    html += "<td><a class=\"searchresults\" href=\"" + item.link + "\">" + file + "</a></td>"; // URL
-    if (item.sizename == "-1 byte") html += "<td></td>"; else html += "<td align=\"right\">" + item.sizename + "</td>"; // Size
-    html += "<td align=\"right\">" + pd + "</td>"; // Date
-    html += "</tr>";
-  }
-  if (type == "image") {
-    var name = title;
-    while ((p = name.indexOf("/")) >= 0) { name = name.substring(p + 1); }
-    html += "<a href=\"" + item.link + "\" name=\"" + name + "\">";
-    html += "<img src=\"" + item.link + "\" width=\"96\" height=\"96\" />";
-    html += "</a>";
-  }
-    
-  // return entry
-  return html;
 }
