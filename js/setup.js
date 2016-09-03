37bd722c9414e2d06500d36c9e8072e10cd56df5
Michael Peter Christen <mc@yacy.net>
2016-09-03 19:11:47 +0200

    changed localhost:8090 to search.yacy.net to make it possible to host this pages on gh-pages

--- js/setup.js
+++ js/setup.js
@@ -6,13 +6,17 @@
  *
  * HOW TO USE:
  * Just change the server address to your own search server address!
+ * For example, if you want to use your local YaCy, set it to
+ *
+ * var server="localhost:8090";
+ *
  * The address may be different from the place where this wab pages application is hosted
  * because the content is fetched using JSONP.
  *
  * After each update, you must change this again or save the address before doing an update.
  */
 
-var server="localhost:8090";
+var server="search.yacy.net";
 var searchURL='http://' + server + '/yacysearch.json?callback=?';
 var suggestUrl='http://' + server + '/suggest.json?callback=?';
 var homepage="http://yacy.net";
