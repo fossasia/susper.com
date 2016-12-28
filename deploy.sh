3c3d09f285f13be7384a0e3f74b68cb1109a9bf4
Nikhil Rayaprolu <nikhil684@gmail.com>
2016-12-28 16:32:45 +0530

    fixing the travis errors

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in id_rsa.enc -out id_rsa -d
 
 chmod 600 deploy_key
 eval `ssh-agent -s`
