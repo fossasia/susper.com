2148e2c4b3d7d0b9933d3bf5c73bdcfd363fc4b3
Nikhil Rayaprolu <nikhil684@gmail.com>
2017-01-30 20:36:36 +0530

    adding sidebar

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -18,12 +18,22 @@ export class SearchService {
   }
   getsearchresults(searchquery) {
     let params = new URLSearchParams();
-    params.set('query', searchquery.query);
-    params.set('rows', searchquery.maximumRecords);
-    params.set('start', searchquery.startRecord);
-    params.set('sort', searchquery.sort);
+    for (let key in searchquery) {
+      if (searchquery.hasOwnProperty(key)) {
+        params.set(key, searchquery[key]);
+      }
+
+    }
     params.set('wt', 'yjson');
     params.set('callback', 'JSONP_CALLBACK');
+
+    params.set('facet', 'true');
+    params.set('facet.mincount', '1');
+    params.append('facet.field', 'host_s');
+    params.append('facet.field', 'url_file_ext_s');
+    params.append('facet.field', 'url_protocol_s');
+    params.append('facet.field', 'author_sxt');
+    params.append('facet.field', 'collection_sxt');
     return this.jsonp
       .get('http://yacy.searchlab.eu/solr/select', {search: params}).map(res => {
         console.log(res.json()[0].channels[0].items);
