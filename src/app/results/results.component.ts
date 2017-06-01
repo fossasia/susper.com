346f9dcbe2570844d0b3638025a65faf3b8a6cba
harshit98 <harshitprasad28@gmail.com>
2017-06-02 02:51:53 +0530

    Fixes #348 Correct hyperlinks are displayed
    Solve lint errors
    
    Enhance filtering of link
    
    Fix linting errors

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -15,6 +15,7 @@ export class ResultsComponent implements OnInit {
   items$: Observable<any>;
   totalResults$: Observable<number>;
   resultDisplay: string;
+  fileFormat: boolean;
   noOfPages: number;
   presentPage: number;
   maxPage: number;
@@ -54,7 +55,7 @@ export class ResultsComponent implements OnInit {
   }
 
   filterByDate() {
-    this.searchdata.sort = 'last_modified desc';
+    this.searchdata.sort = 'last_modified desc'; // last modified
     this.route.navigate(['/search'], { queryParams: this.searchdata });
   }
 
@@ -128,12 +129,11 @@ export class ResultsComponent implements OnInit {
       } else {
         this.resultDisplay = 'all';
       }
+
       if (query['resultDisplay']) {
         this.resultDisplay = query['resultDisplay'];
         this.searchdata.resultDisplay = this.resultDisplay;
-
       }
-
 
 
       this.presentPage = Math.abs(query['start'] / this.searchdata.rows) + 1;
@@ -149,10 +149,12 @@ export class ResultsComponent implements OnInit {
       this.store.dispatch(new queryactions.QueryServerAction(query));
       this.items$ = store.select(fromRoot.getItems);
       this.totalResults$ = store.select(fromRoot.getTotalResults);
+
       this.totalResults$.subscribe(totalResults => {
         if (totalResults) {
           this.hidefooter = 0;
         }
+
         this.end = Math.min(totalResults, this.begin + this.searchdata.rows - 1);
         this.message = 'showing results ' + this.begin + ' to ' + this.end + ' of ' + totalResults;
         this.noOfPages = Math.ceil(totalResults / this.searchdata.rows);
@@ -163,10 +165,10 @@ export class ResultsComponent implements OnInit {
       this.presentPage = Math.abs(query['start'] / this.searchdata.rows) + 1;
 
     });
+
     this.totalResults$.subscribe(totalResults => {
       if (totalResults) {
         this.hidefooter = 0;
-
       }
 
       this.end = Math.min(totalResults, this.begin + this.searchdata.rows - 1);
@@ -174,11 +176,27 @@ export class ResultsComponent implements OnInit {
       this.noOfPages = Math.ceil(totalResults / this.searchdata.rows);
       this.maxPage = Math.min(this.searchdata.rows, this.noOfPages);
     });
-
-
   };
 
   ngOnInit() {
 
+  }
+
+  filterLink(link) {
+    if (link.includes('jpg')) {
+      this.fileFormat = false;
+      return false;
+    } else {
+      this.fileFormat = true;
+      return true;
+    }
+  }
+
+  displayNoDate(description) {
+    if (this.fileFormat === false) {
+      return false;
+    } else {
+      return description;
+    }
   }
 }
