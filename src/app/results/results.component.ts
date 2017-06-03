cff10afa0616d96c478e62407f686915ec2c8408
Nikhil Rayaprolu <nikhil684@gmail.com>
2017-06-03 15:40:10 +0530

    Fixes  #388 , #389 , #404 , #395 now pagination bar is displayed even when user directly enters search query on URL bar (#405)
    * Fixes #404 now pagination bar is displayed
    
    * removing lint errors

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -138,7 +138,12 @@ export class ResultsComponent implements OnInit {
 
 
 
-      this.presentPage = Math.abs(query['start'] / this.searchdata.rows) + 1;
+      if (query['start']) {
+        this.searchdata.start = query['start'];
+      } else {
+        this.searchdata.start = 0;
+      }
+
       this.searchdata.query = query['query'];
       this.store.dispatch(new queryactions.QueryAction(query['query']));
       this.querylook = Object.assign({}, query);
@@ -156,7 +161,7 @@ export class ResultsComponent implements OnInit {
           this.hidefooter = 0;
         }
         this.end = Math.min(totalResults, this.begin + this.searchdata.rows - 1);
-        this.message = 'showing results ' + this.begin + ' to ' + this.end + ' of ' + totalResults;
+        this.message =  'showing results ' + this.begin + ' to ' + this.end + ' of ' + totalResults;
         this.noOfPages = Math.ceil(totalResults / this.searchdata.rows);
         this.maxPage = Math.min(this.searchdata.rows, this.noOfPages);
       });
@@ -172,7 +177,7 @@ export class ResultsComponent implements OnInit {
       }
 
       this.end = Math.min(totalResults, this.begin + this.searchdata.rows - 1);
-      this.message = 'showing results ' + this.begin + ' to ' + this.end + ' of ' + totalResults;
+      this.message =  'showing results ' + this.begin + ' to ' + this.end + ' of ' + totalResults;
       this.noOfPages = Math.ceil(totalResults / this.searchdata.rows);
       this.maxPage = Math.min(this.searchdata.rows, this.noOfPages);
     });
