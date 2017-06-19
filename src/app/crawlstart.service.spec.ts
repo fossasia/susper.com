b7a30af4168dd8472c8805d602d572b52822ca90
Nikhil Rayaprolu <nikhil684@gmail.com>
2017-06-19 11:26:53 +0530

    Addresses #413 crawl start UI implemented (#415)
    * Ref to issue #413 crawl start UI implemented
    
    * add crawljob in footer
    
    * removing codacy errors
    
    * errors removed
    
    * added authentication
    
    * final fixes
    
    * changed localhost to yacy

---
+++ src/app/crawlstart.service.spec.ts
@@ -0,0 +1,13 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { CrawlstartService } from './crawlstart.service';
+
+describe('CrawlstartService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [CrawlstartService]
+    });
+  });
+
+
+});
