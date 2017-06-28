e37a8834e4aaff06a96ab689f25ab8f76d5f9c74
Nikhil Rayaprolu <nikhil684@gmail.com>
2017-06-28 23:51:19 +0530

    Fixes #564 now susper answers questions

---
+++ src/app/intelligence.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {URLSearchParams, Http, Jsonp} from "@angular/http";
+import {Store} from "@ngrx/store";
+import * as fromRoot from './reducers';
+import {Observable} from "rxjs";
+
+@Injectable()
+export class IntelligenceService {
+  server = 'http://api.susi.ai';
+  searchURL = 'http://' + this.server + '/susi/chat.json';
+  constructor(private http: Http, private jsonp: Jsonp, private store: Store<fromRoot.State>) {
+  }
+  getintelligentresponse(searchquery) {
+    let params = new URLSearchParams();
+    params.set('q', searchquery);
+    // params.set('QueryClass', 'MaxHits=5');
+
+    params.set('callback', 'JSONP_CALLBACK');
+
+    return this.jsonp
+      .get('http://api.asksusi.com/susi/chat.json', {search: params}).map(res =>
+        res.json()
+
+      );
+  }
+
+
+
+  private handleError (error: any) {
+    // In some advance version we can include a remote logging of errors
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // Right now we are logging to console itself
+    return Observable.throw(errMsg);
+  }
+}
