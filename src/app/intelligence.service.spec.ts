c279618d55b00f0b47c493c9e3b2b616d968a516
Nikhil Rayaprolu <nikhil684@gmail.com>
2017-06-29 04:32:24 +0530

    Fixes #512 Fixed travis errors (#574)

--- src/app/intelligence.service.spec.ts
+++ src/app/intelligence.service.spec.ts
@@ -1,10 +1,20 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { IntelligenceService } from './intelligence.service';
+import {HttpModule, JsonpModule} from "@angular/http";
+import {StoreModule} from "@ngrx/store";
+import {reducer} from "./reducers/index";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 
 describe('IntelligenceService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpModule,
+        JsonpModule,
+        StoreModule.provideStore(reducer),
+        StoreDevtoolsModule.instrumentOnlyWithExtension(),
+      ],
       providers: [IntelligenceService]
     });
   });
