eb8b36a95fd07f721389b43887665853be7d0bd5
Soumya Vadlamannati <vadlamannati1997@gmail.com>
2017-06-29 12:35:23 +0530

    Adding basic tests for results component (#560)
    Additional tests
    
    More changes
    
    Solved linting errors

--- src/app/results/results.component.spec.ts
+++ src/app/results/results.component.spec.ts
@@ -96,4 +96,77 @@ describe('ResultsComponent', () => {
     expect(compiled.querySelector('app-advancedsearch')).toBeTruthy();
   });
 
+  it('should have an app-related-search element', () => {
+    let compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('app-related-search')).toBeTruthy();
+  });
+
+  it('should have an footer-navbar element', () => {
+    let compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('app-footer-navbar')).toBeTruthy();
+  });
+
+  it('should have an app-infobox element', () => {
+    let compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('app-infobox')).toBeTruthy();
+  });
+
+  it('should have an app-theme element', () => {
+    let compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('app-theme')).toBeTruthy();
+  });
+
+
+  it('should have a search options menu', () => {
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('div#search-options-field ul#search-options'));
+  });
+
+  it('should have "items$" variable', () => {
+    expect(component.items$).toBeTruthy();
+  });
+
+  it('should have a tool drop-down menu', () => {
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('ul#tool-dropdown'));
+  });
+
+  it('should have a tool drop-down menu', () => {
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('ul#setting-dropdown'));
+  });
+
+  it('should have a pagination bar', () => {
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('div.pagination-bar'));
+  });
+
+  it('should have correctly related sub-components', () => {
+    let compiled = fixture.debugElement.nativeElement;
+    if (component.resultDisplay.toLocaleLowerCase() === 'all') {
+      expect(compiled.querySelector('div.text-result div.title'));
+      expect(compiled.querySelector('div.text-result div.link'));
+      expect(compiled.querySelector('div.text-result div.description'));
+    } else if (component.resultDisplay.toLocaleLowerCase() === 'images') {
+      expect(compiled.querySelector('div.grid div.cell'));
+      expect(compiled.querySelector('div.grid div.cell a.image-pointer'));
+    } else if (component.resultDisplay.toLocaleLowerCase() === 'videos') {
+      expect(compiled.querySelector('div.video-result div.title'));
+      expect(compiled.querySelector('div.video-result div.link'));
+    }
+  });
+
+  it('should have appropriate message', () => {
+    let compiled = fixture.debugElement.nativeElement;
+    if (component.totalNumber < 1) {
+      expect(compiled.querySelector('div.noResults'));
+    } else if  (component.totalNumber > 0) {
+      expect(compiled.querySelector('div.message-bar'));
+    }
+  });
+
 });
