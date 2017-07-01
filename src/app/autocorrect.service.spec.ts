b8f1f69e430a14358feddac32888b9088d5b62d1
Soumya Vadlamannati <vadlamannati1997@gmail.com>
2017-07-02 00:43:36 +0530

    Resolving merge and travis errors

--- src/app/autocorrect.service.spec.ts
+++ src/app/autocorrect.service.spec.ts
@@ -2,9 +2,20 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { AutoCorrectService } from './autocorrect.service';
 
+import {HttpModule, JsonpModule} from "@angular/http";
+import {StoreModule} from "@ngrx/store";
+import {reducer} from "./reducers/index";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+
 describe('AutoCorrectService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpModule,
+        JsonpModule,
+        StoreModule.provideStore(reducer),
+        StoreDevtoolsModule.instrumentOnlyWithExtension(),
+      ],
       providers: [AutoCorrectService]
     });
   });
