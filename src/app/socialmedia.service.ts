e8e414720d7aa6c6e84aea5945cbbabd93c331e0
Nikhil Rayaprolu <nikhil684@gmail.com>
2017-07-01 19:31:45 +0530

    second changes

--- src/app/socialmedia.service.ts
+++ src/app/socialmedia.service.ts
@@ -5,30 +5,46 @@ import {Observable} from "rxjs";
 import * as fromRoot from './reducers';
 @Injectable()
 export class SocialmediaService {
-  server = 'http://lookup.dbpedia.org';
-  searchURL = this.server + '/api/search/KeywordSearch?';
-  homepage = 'http://susper.com';
-  logo = '../images/susper.svg';
-  constructor(private http: Http, private jsonp: Jsonp, private store: Store<fromRoot.State>) {
+  apiUrl: URL = new URL('http://api.loklak.org/api/search.json');
+  maximum_records_fetch = 20;
+  minified_results = true;
+  source = 'all';
+  fields = 'created_at,screen_name,mentions,hashtags';
+  limit = 10;
+  timezoneOffset: string = new Date().getTimezoneOffset().toString();
+
+  constructor(
+    private jsonp: Jsonp
+  ) { }
+
+  public fetchQuery(query: string, lastRecord = 0) {
+    const searchParams = new URLSearchParams();
+    searchParams.set('q', query);
+    searchParams.set('callback', 'JSONP_CALLBACK');
+    searchParams.set('minified', this.minified_results.toString());
+    searchParams.set('source', this.source);
+    searchParams.set('maximumRecords', this.maximum_records_fetch.toString());
+    searchParams.set('timezoneOffset', this.timezoneOffset);
+    searchParams.set('startRecord', (lastRecord + 1).toString());
+    searchParams.set('fields', this.fields);
+    searchParams.set('limit', this.limit.toString());
+    return this.jsonp.get(this.apiUrl.toString(), { search: searchParams })
+      .map(this.extractData)
+      .catch(this.handleError);
+
   }
-  getsearchresults(searchquery) {
 
-    let params = new URLSearchParams();
-    params.set('QueryString', searchquery);
-
-    let headers = new Headers({ 'Accept': 'application/json' });
-    let options = new RequestOptions({ headers: headers, search: params });
-    return this.http
-      .get(this.searchURL, options).map(res =>
-
-        res.json()
-
-      ).catch(this.handleError);
-
+  private extractData(res) {
+    try {
+      return res.json();
+    } catch (error) {
+      console.error(error);
+    }
   }
-  private handleError (error: any) {
+
+  private handleError(error: any) {
     // In some advance version we can include a remote logging of errors
-    let errMsg = (error.message) ? error.message :
+    const errMsg = (error.message) ? error.message :
       error.status ? `${error.status} - ${error.statusText}` : 'Server error';
     console.error(errMsg); // Right now we are logging to console itself
     return Observable.throw(errMsg);
