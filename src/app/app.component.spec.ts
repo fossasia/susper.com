9bbd81436e919a7acb6640ba39cbacfdb62df125
Nikhil Rayaprolu <nikhil684@gmail.com>
2017-07-02 00:07:53 +0530

    minor lint changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ import {RelatedSearchComponent} from "./related-search/related-search.component"
 import {AutoCompleteComponent} from "./auto-complete/auto-complete.component";
 import { ThemeComponent } from './theme/theme.component';
 import { DropdownComponent } from './dropdown/dropdown.component';
-import {SocialmediaComponent} from "./socialmedia/socialmedia.component";
 import {MediaWallComponent} from "./media-wall/media-wall.component";
 import {MediaWallLinkerComponent} from "./wall-linker/wall-linker.component";
 import {IntelligenceComponent} from "./intelligence/intelligence.component";
+import {SocialmediaComponent} from "./socialmedia/socialmedia.component";
 
 describe('AppComponent', () => {
   beforeEach(() => {
@@ -63,7 +63,7 @@ describe('AppComponent', () => {
         DropdownComponent,
         SocialmediaComponent,
         MediaWallComponent,
-        MediaWallLinkerComponent
+        MediaWallLinkerComponent,
         IntelligenceComponent
       ]
     });
