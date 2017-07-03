4c69243795a2dc5110c539a1d264f02bb2414371
harshit98 <harshitprasad28@gmail.com>
2017-07-03 17:17:05 +0530

    add leaflet types

--- src/app/map.service.spec.ts
+++ src/app/map.service.spec.ts
@@ -1,16 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { HttpModule } from '@angular/http';
 import { MapService } from './map.service';
-import { Map } from 'leaflet';
-import L from 'leaflet';
 
 describe('MapService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         HttpModule,
-        Map,
-        L
       ],
       providers: [
         MapService
