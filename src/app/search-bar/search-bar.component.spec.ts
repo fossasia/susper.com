78f222d65d6c5759533829f4fd06f334785c3ce2
Nikhil Rayaprolu <nikhil684@gmail.com>
2017-07-06 19:11:44 +0530

    travis fixed

--- src/app/search-bar/search-bar.component.spec.ts
+++ src/app/search-bar/search-bar.component.spec.ts
@@ -14,6 +14,7 @@ import {SocialmediaComponent} from "../socialmedia/socialmedia.component";
 import {MediaWallComponent} from "../media-wall/media-wall.component";
 import {MediaWallLinkerComponent} from "../wall-linker/wall-linker.component";
 import {IntelligenceComponent} from "../intelligence/intelligence.component";
+import {ThemeComponent} from "../theme/theme.component";
 
 describe('Component: SearchBarComponent', () => {
   let component: SearchBarComponent;
