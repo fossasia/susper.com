c6e30426c567c9ab93c58e257b57ae270a4a92d0
Soumya Vadlamannati <vadlamannati1997@gmail.com>
2017-08-08 20:55:08 +0530

    Fixes #633, auto correct aligned for index page, search bar design changed (#723)
    Fixed linting errors

--- src/app/auto-complete/auto-complete.component.ts
+++ src/app/auto-complete/auto-complete.component.ts
@@ -13,6 +13,7 @@ import * as fromRoot from '../reducers';
 export class AutoCompleteComponent implements OnInit {
   results: Array<any>;
   query$: any;
+  id: string;
   resultsearch = '/search';
 
   @Output() hidecomponent: EventEmitter<any> = new EventEmitter<any>();
@@ -49,6 +50,14 @@ export class AutoCompleteComponent implements OnInit {
         });
       }
     });
+  }
+
+  getID() {
+    if ( this.route.url.toString() === '/') {
+      return 'index-sug-box';
+    } else {
+      return 'sug-box'
+    }
   }
 
   ngOnInit() {
