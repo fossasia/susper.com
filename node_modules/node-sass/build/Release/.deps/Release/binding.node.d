53d1adf82d74b4baed5a1fed15fe7965e5c31d48
Travis CI User <travis@example.org>
2017-08-21 22:48:24 +0000

    Travis CI Clean Deploy : 8075552259078445556a4403ab591b359694bc8c

--- node_modules/node-sass/build/Release/.deps/Release/binding.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/binding.node := rm -rf "Release/binding.node" && cp -af "Release/obj.target/binding.node" "Release/binding.node"
--- node_modules/node-sass/build/Release/.deps/Release/sass.a.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/sass.a := rm -rf "Release/sass.a" && cp -af "Release/obj.target/src/sass.a" "Release/sass.a"
