03162be279143c0341dbaf56d35aae148763403a
Valentin Bercaru <valentin_bercaru@yahoo.com>
2018-02-26 23:02:05 +0200

    Fixed #843 and #914 : Added a more complex night theme

--- src/app/theme/theme.component.ts
+++ src/app/theme/theme.component.ts
@@ -35,6 +35,9 @@ export class ThemeComponent implements OnInit {
       case "defaultTheme": {
         return this.defaultTheme();
       }
+      case "nightTheme": {
+        return this.nightTheme();
+      }
       default: {
         return;
       }
@@ -58,6 +61,21 @@ export class ThemeComponent implements OnInit {
     this.themeService.titleColor = '#1a0dab';
     this.themeService.linkColor = '#006621';
     this.themeService.descriptionColor = '#545454';
+    (document.getElementsByClassName("infobox")[0] as HTMLElement).style.color = '#252f41';
+    (document.getElementsByClassName("navbar-header")[0] as HTMLElement).style.background = '#f8f8f8';
+    (document.getElementsByClassName("container-fluid")[0] as HTMLElement).style.background = '#f8f8f8';
+    (document.getElementsByClassName("container-fluid")[1] as HTMLElement).style.background = '#f8f8f8';
+    (document.getElementsByClassName("footer-bar")[0] as HTMLElement).style.background = '#f2f2f2';
+    (document.getElementsByClassName("footer-bar")[0] as HTMLElement).style.borderTop = '#e4e4e4';
+    (document.getElementsByClassName("input-group")[0] as HTMLElement).style.background = '#ffffff';
+    (document.getElementById("nav-input") as HTMLElement).style.background = '#ffffff';
+    (document.getElementById("nav-input") as HTMLElement).style.color = '#000000';
+    (document.getElementById("speech-button") as HTMLElement).style.background = '#ffffff';
+    $(document).ready(function() {
+      (document.getElementById("nav-button") as HTMLElement).style.background = '#ffffff';
+      (document.getElementsByClassName("mean")[0] as HTMLElement).style.color = "#363636";
+      (document.getElementsByClassName("card")[2] as HTMLElement).style.color = "#363636";      
+    });
   }
 
   basicTheme() {
@@ -79,5 +97,27 @@ export class ThemeComponent implements OnInit {
     this.themeService.titleColor = '#00ff00';
     this.themeService.linkColor = '#FFFFFF  ';
     this.themeService.descriptionColor = '#F1C40F';
+  }
+
+  nightTheme() {
+    this.themeService.backgroundColor = '#222222';
+    this.themeService.titleColor = '#5a9e26';
+    this.themeService.linkColor = '#42a2f4';
+    this.themeService.descriptionColor = '#dddddd';
+    (document.getElementsByClassName("infobox")[0] as HTMLElement).style.color = '#ffffff';
+    (document.getElementsByClassName("navbar-header")[0] as HTMLElement).style.background = '#373737';
+    (document.getElementsByClassName("container-fluid")[0] as HTMLElement).style.background = '#373737';
+    (document.getElementsByClassName("container-fluid")[1] as HTMLElement).style.background = '#373737';
+    (document.getElementsByClassName("footer-bar")[0] as HTMLElement).style.background = '#373737';
+    (document.getElementsByClassName("footer-bar")[0] as HTMLElement).style.border = '#373737';
+    (document.getElementsByClassName("input-group")[0] as HTMLElement).style.background = '#323232';
+    (document.getElementById("nav-input") as HTMLElement).style.background = '#323232';
+    (document.getElementById("nav-input") as HTMLElement).style.color = '#ffffff';
+    (document.getElementById("speech-button") as HTMLElement).style.background = '#323232';
+    $(document).ready(function() {
+      (document.getElementById("nav-button") as HTMLElement).style.background = '#323232';
+      (document.getElementsByClassName("mean")[0] as HTMLElement).style.color = "#dddddd";
+      (document.getElementsByClassName("card")[2] as HTMLElement).style.color = "#dddddd";      
+    });
   }
 }
