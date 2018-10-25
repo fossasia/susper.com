64977e35124dbc905d4889d8a209bc3375954efc
Prabhu Pant <prabhupant09@gmail.com>
2018-10-25 20:37:33 +0530

    Add variable for yacy site

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,6 +8,7 @@ import { url } from '../../assets/url_configuration';
 export class AboutComponent implements OnInit {
   susperUrl = url.susper.site;
   yacyUrl = url.yacy.api_server;
+  yacySite = url.yacy.site;
   fossasia_repo = url.github_repo.fossasia;
   constructor() { }
   ngOnInit() {
