10ce77345a09305079fcd6858fe3b872fc7908f5
Shreyansh Dwivedi <shreyanshdwivedi1997@gmail.com>
2018-12-09 00:00:19 +0530

    Fixed formatting of code

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, OnInit, ChangeDetectorRef, DoCheck } from '@angular/core';
+import { Component, ViewChild, OnInit, DoCheck } from '@angular/core';
 import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
 @Component({
     selector: 'app-contact',
