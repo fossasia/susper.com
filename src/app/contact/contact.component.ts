81e29792e215edc7329ee64db8daa5683f35cfdf
sk9331657@gmail.com <sk9331657@gmail.com>
2018-12-09 21:46:42 +0530

    Contact Form Now Working Fix-1217

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
+
 @Component({
     selector: 'app-contact',
     templateUrl: './contact.component.html',
@@ -9,11 +10,10 @@ export class ContactComponent implements OnInit {
 
     @ViewChild('submitButton') submitButton;
     @ViewChild('emailInput') emailInput;
-
     nameInput: string;
     tpnoInput: number;
     contactMessage: string = '';
-
+    countrycode: string;
     @ViewChild('myModal')
     modal: ModalComponent;
 
@@ -42,6 +42,19 @@ export class ContactComponent implements OnInit {
             this.submitButton.nativeElement.disabled = false;
         } else {
             this.submitButton.nativeElement.disabled = true;
+        }
+    }
+    mail() {
+        if (this.tpnoInput && this.tpnoInput.toString().length >= 10 && this.tpnoInput > 0 && this.contactMessage && this.contactMessage.length >= 100) {
+        let Message = "Name:   " + this.nameInput + "     Contact No:    " + this.countrycode + this.tpnoInput + '       ' + "Message:       " + this.contactMessage;
+        let js1 = document.createElement("script");
+        js1.innerHTML = "Email.send({SecureToken : 'e64d9c8a-8ce6-46f1-8d1c-3b9acebe5c20' ,To:'office@fossasia.org' ,From:'" + this.emailInput + "',Subject:'Contact Form', Body:'" + Message + "'}).then( function(message){if(message === 'OK'){alert('sent')} else {alert('Not Sent.Retry')}});"
+        js1.type = "text/javascript" ;
+
+        document.body.appendChild(js1);
+        this.modal.close();
+        } else {
+            alert('Error in Details');
         }
     }
 }
