7b5cb923b2120b331cc26dbfbae4e6533e5d7e3e
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2018-12-20 23:40:19 +0530

    Fixes #1327: Contact componnet refactored

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,78 +1,49 @@
-import { Component, ViewChild, OnInit, DoCheck } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
+import { validatePhone, validateEmail, validateMessage, validateName, countryCodeList } from '../utils';
+
 @Component({
-    selector: 'app-contact',
-    templateUrl: './contact.component.html',
-    styleUrls: ['./contact.component.css']
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css'],
 })
-export class ContactComponent implements OnInit, DoCheck {
+export class ContactComponent implements OnInit {
+  @ViewChild('submitButton') submitButton;
 
-    @ViewChild('submitButton') submitButton;
+  emailInput: string;
+  nameInput: string;
+  phoneNumberInput: number;
+  contactMessage: string;
+  countryCodeList: any;
 
-    emailInput: string;
-    nameInput: string;
-    tpnoInput: number;
-    contactMessage: string = '';
+  @ViewChild('ContactUsModal')
+  modal: ModalComponent;
 
-    @ViewChild('myModal')
-    modal: ModalComponent;
+  constructor() {}
 
-    constructor() { }
+  ngOnInit() {
+    this.contactMessage = '';
+    this.countryCodeList = countryCodeList;
+  }
 
-    ngOnInit() {
+  closeModal() {
+    this.modal.close();
+  }
+
+  openModal() {
+    this.modal.open();
+  }
+
+  checkValidity() {
+    if (
+      validateEmail(this.emailInput) &&
+      validatePhone(this.phoneNumberInput) &&
+      validateMessage(this.contactMessage) &&
+      validateName(this.nameInput)
+    ) {
+      this.submitButton.nativeElement.disabled = false;
+    } else {
+      this.submitButton.nativeElement.disabled = true;
     }
-
-    close() {
-        this.modal.close();
-    }
-
-    open() {
-        this.modal.open();
-    }
-
-    ngDoCheck() {
-        this.checkValidity();
-    }
-
-    checkValidity() {
-        if (this.validateEmail(this.emailInput) && this.validatePhone(this.tpnoInput) && this.validateMessage(this.contactMessage) && this.validateName(this.nameInput)) {
-            this.submitButton.nativeElement.disabled = false;
-        } else {
-            this.submitButton.nativeElement.disabled = true;
-        }
-    }
-
-    validateMessage(message) {
-        if (message && (message.length >= 100)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    validatePhone(phone) {
-        if (phone && (phone.toString().length >= 10)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    validateEmail(email) {
-        if (email) {
-            let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return re.test(email);
-        } else {
-            return false;
-        }
-    }
-
-    validateName(name) {
-        if (name && (name.length > 0)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
+  }
 }
