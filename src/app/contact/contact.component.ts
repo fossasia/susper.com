a134e558954517650e66b09bf4222b469b3b3fb7
sk9331657@gmail.com <sk9331657@gmail.com>
2018-12-30 15:00:05 +0530

    Fix Contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, OnInit, DoCheck } from '@angular/core';
 import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
+import {environment} from 'environments/environment'
 @Component({
     selector: 'app-contact',
     templateUrl: './contact.component.html',
@@ -8,16 +9,23 @@ import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
 export class ContactComponent implements OnInit, DoCheck {
 
     @ViewChild('submitButton') submitButton;
+    token = environment.token;
 
     emailInput: string;
     nameInput: string;
     tpnoInput: number;
+    countrycode: string;
     contactMessage: string = '';
 
     @ViewChild('myModal')
     modal: ModalComponent;
 
-    constructor() { }
+    constructor() {
+        if(this.token === "") {
+            console.log("Token Missing")
+            return;
+        }
+     }
 
     ngOnInit() {
     }
@@ -73,6 +81,19 @@ export class ContactComponent implements OnInit, DoCheck {
         } else {
             return false;
         }
+    }
+    mail () {
+        if (this.tpnoInput && this.tpnoInput.toString().length >= 10 && this.tpnoInput > 0 && this.contactMessage && this.contactMessage.length >= 100 ) {
+            let Message = "Name:   " + this.nameInput + "     Contact No:    " + this.countrycode + this.tpnoInput + '       ' + "Message:       " + this.contactMessage;
+            let js1 = document.createElement("script");
+            let ScriptHTML = "Email.send({SecureToken : '"+this.token+"' ,To:'office@fossasia.org' ,From:'" + this.emailInput + "',Subject:'Contact Form', Body:'" + Message + "'}).then(function(message){if(message === 'OK'){ alert('sent'); } else {alert('Retry'); }});";
+            js1.innerHTML = ScriptHTML;
+            js1.type = "text/javascript" ;
+            this.modal.close();
+            document.body.appendChild(js1);
+            } else {
+                alert('Error in Details or You are in development Mode');
+            }
     }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+    //Put token here
+    token : ""
 };
