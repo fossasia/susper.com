2b126653ce29b1bac2fe6ea5c2ff6be143171678
Ambuj <thecoderguy@outlook.com>
2019-01-06 17:07:15 +0530

    fixing port number to be printed on console and fixed a comment typo

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
+// Serve only the static files from the dist directory
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
-    
+
 res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+port = process.env.PORT || 8080;
+app.listen(port, ()=>{
+  console.log("Server Started at PORT: "+port);
+});
