5221a578dd24c1cc8d89604e8806d217315191c9
humble_D <aakashs.mallik.cse15@itbhu.ac.in>
2019-01-27 19:03:49 +0530

    Fixes  #1317: Reducers and Actions cleaned (#1318)

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,7 +1,6 @@
 import { createSelector } from 'reselect';
 import { ActionReducer } from '@ngrx/store';
 import { environment } from '../../environments/environment';
-import * as fromKnowledge from './knowledge';
 
 /**
  * The compose function is one of our most handy tools. In basic terms, you give
@@ -39,6 +38,7 @@ import { combineReducers } from '@ngrx/store';
 import * as fromSearch from './search';
 import * as fromQuery from './query';
 import * as fromSpeech from './speech';
+import * as fromKnowledge from './knowledge';
 /**
  * As mentioned, we treat each reducer like a table in a database. This means
  * our top level state interface is just a map of keys to inner state types.
@@ -64,10 +64,12 @@ const reducers = {
   knowledge: fromKnowledge.reducer,
 };
 
+
 const developmentReducer: ActionReducer<State> = compose(
   storeFreeze,
   combineReducers
 )(reducers);
+
 const productionReducer: ActionReducer<State> = combineReducers(reducers);
 
 export function reducer(state: any, action: any) {
@@ -85,9 +87,14 @@ export const getSpeechState = (state: State) => state.speech;
 
 export const getSearchResults = createSelector(getSearchState, fromSearch.getsearchresults);
 export const getItems = createSelector(getSearchState, fromSearch.getItems);
+export const getResponseTime = createSelector(getSearchState, fromSearch.getresponsetime);
 export const getTotalResults = createSelector(getSearchState, fromSearch.getTotalResults);
 export const getNavigation = createSelector(getSearchState, fromSearch.getNavigation);
+export const getSearchStatus = createSelector(getSearchState, fromSearch.getSearchstatus);
+
 export const getquery = createSelector(getQueryState, fromQuery.getpresentquery);
+export const getwholequery = createSelector(getQueryState, fromQuery.getpresentwholequery);
+
 export const getKnowledgeContent = createSelector(
   getKnowledgeState,
   fromKnowledge.getContentResponse
@@ -97,7 +104,5 @@ export const getDescription = createSelector(
   getKnowledgeState,
   fromKnowledge.getDescriptionResponse
 );
-export const getwholequery = createSelector(getQueryState, fromQuery.getpresentwholequery);
-export const getResponseTime = createSelector(getSearchState, fromSearch.getresponsetime);
+
 export const getSpeechMode = createSelector(getSpeechState, fromSpeech.getspeechmode);
-export const getSearchStatus = createSelector(getSearchState, fromSearch.getSearchstatus);
