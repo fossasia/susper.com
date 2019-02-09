d6d1c6fb5dae0e9a5a8fb50fd4bd48918e46e693
subhahu <34541684+subhahu123@users.noreply.github.com>
2019-02-09 22:52:08 +0530

    Update autocorrect.service.spec.ts

--- src/app/services/autocorrect.service.spec.ts
+++ src/app/services/autocorrect.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed, inject } from '@angular/core/testing';
 import {HttpModule, JsonpModule} from "@angular/http";
 import {StoreModule} from "@ngrx/store";
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 import {reducer} from "../reducers/index";
 import {AutocorrectService} from "./autocorrect.service";
 
