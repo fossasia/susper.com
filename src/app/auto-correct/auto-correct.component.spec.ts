fd5c5c3b2d1c001d35c54bd8575da85fcaa56aec
subhahu <34541684+subhahu123@users.noreply.github.com>
2019-02-09 22:41:42 +0530

    Update auto-correct.component.spec.ts

--- src/app/auto-correct/auto-correct.component.spec.ts
+++ src/app/auto-correct/auto-correct.component.spec.ts
@@ -5,6 +5,7 @@ import {StoreModule} from "@ngrx/store";
 
 import { AutoCorrectComponent } from './auto-correct.component';
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+import { environment } from '../environments/environment';
 import {FormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
 import {BrowserModule} from "@angular/platform-browser";
@@ -27,7 +28,7 @@ describe('AutoCorrectComponent', () => {
               HttpModule,
               JsonpModule,
               StoreModule.provideStore(reducer),
-              StoreDevtoolsModule.instrumentOnlyWithExtension(),
+              !environment.production ? StoreDevtoolsModule.instrument({ maxAge: 50 }) : [],
             ],
             declarations: [
               AutoCorrectComponent,
