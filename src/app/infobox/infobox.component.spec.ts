dfe5ceb3bdc593501cddc2bdf23991974286cbdd
subhahu <34541684+subhahu123@users.noreply.github.com>
2019-02-10 00:32:49 +0530

    Update infobox.component.spec.ts

--- src/app/infobox/infobox.component.spec.ts
+++ src/app/infobox/infobox.component.spec.ts
@@ -20,7 +20,7 @@ describe('Component: InfoboxComponent', () => {
         RouterTestingModule,
         HttpModule,
         JsonpModule,
-        StoreModule.forFeature(reducer)
+        StoreModule.forRoot(reducer)
       ],
       declarations: [
         InfoboxComponent
