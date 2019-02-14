2d2471c7506c126a1b9565b1e3a841de09d075ee
subhahu <34541684+subhahu123@users.noreply.github.com>
2019-02-15 01:20:09 +0530

    Update intelligence.service.spec.ts

--- src/app/services/intelligence.service.spec.ts
+++ src/app/services/intelligence.service.spec.ts
@@ -13,7 +13,7 @@ describe('IntelligenceService', () => {
         HttpModule,
         JsonpModule,
         StoreModule.forRoot(reducer),
-        !environment.production ? StoreDevtoolsModule.instrument({ maxAge: 50 }) : [],
+        !environment.production ? StoreDevtoolsModule.instrument() : [],
       ],
       providers: [IntelligenceService]
     });
