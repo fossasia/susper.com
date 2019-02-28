5841d3d46c73b471f70debba982d1effadb59090
subhahu <34541684+subhahu123@users.noreply.github.com>
2019-02-28 23:29:21 +0530

    Update url_configuration.ts

--- src/assets/url_configuration.ts
+++ src/assets/url_configuration.ts
@@ -1,47 +1,86 @@
 export const url = {
         'susper': {
+                'img': {
+                        'png': ''
+                },
                 'site': 'susper.com',
                 'newsFile': 'https://github.com/fossasia/susper.com/blob/master/src/assets/newsFile.json'
         },
         'yacy': {
+                'img': {
+                        'png': ''
+                },
                 'site': 'https://yacy.net',
                 'api_server': 'yacy.searchlab.eu',
                 'crawl_start_api': 'yacygrid.com:8300',
                 'websearch_api': 'yacy-websearch.net'
         },
         'fossasia': {
+                'img': {
+
+                },
                 'site': 'fossasia.org',
                 'blog': 'blog.fossasia.org',
                 'labs': 'labs.fossasia.org'
         },
         'loklak': {
+                'img': {
+                        'png': ''
+                },
                 'site': 'loklak.org'
         },
         'susi': {
+                'img': {
+                        'png': ''
+                },
                 'site': 'chat.susi.ai'
         },
         'eventyay': {
+                'img': {
+                        'png': ''
+                },
                 'site': 'eventyay.com'
         },
         'pslab': {
+                'img': {
+                        'png': ''
+                },
                 'site': 'pslab.io'
         },
         'badgeyay': {
+                'img': {
+                        'png': ''
+                },
                 'site': 'http://badgeyay.com'
         },
         'meilix': {
+                'img': {
+                        'png': ''
+                },
                 'site': 'meilix.org'
         },
         'phimpme': {
+                'img': {
+                        'png': ''
+                },
                 'site': 'https://phimp.me/'
         },
         'susimagicmirror': {
+                'img': {
+                        'png': ''
+                },
                 'site': 'https://github.com/fossasia/MMM-SUSI-AI'
         },
         'yaydoc': {
+                'img': {
+                        'png': ''
+                },
                 'site': 'http://dev.yaydoc.org'
         },
         'github_repo': {
+                'img': {
+                        'png': ''
+                },
                 'fossasia': 'github.com/fossasia'
         }
 
